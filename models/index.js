b8e4d06fe88ba0ee2959dbe9d816dcb28ceaeb51
RufatEskendarov <rufat.eskendarov@gmail.com>
2023-02-26 23:51:01 -0400

    Updated models

--- models/index.js
+++ models/index.js
@@ -16,22 +16,24 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-// Product.belongsToMany(ProductTag, {
-//   foreignKey: "product_id",
-// });
-// ProductTag.hasMany(Product, {
-//   foreignKey: "product_id",
-//   onDelete: "CASCADE",
-// });
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
 
-// // Tags belongToMany Products (through ProductTag)
-// Tag.belongsToMany(ProductTag, {
-//   foreignKey: "tag_id",
-// });
-// ProductTag.hasMany(Tag, {
-//   foreignKey: "tag_id",
-//   onDelete: "CASCADE",
-// });
+  as: "tags",
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+
+  as: "products",
+});
 
 module.exports = {
   Product,
